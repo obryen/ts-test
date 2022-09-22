0489a3b65a38e69aa44a9b9a211be0dac6027cab
Bryan <bryantoromo@gmail.com>
2022-09-22 17:33:55 +0300

    implement getArnieQuotes

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,40 @@
 import { httpGet } from './mock-http-interface';
 
 // TODO define this type properly
-type TResult = any;
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+export type TResult = {
+  "Arnie Quote"?: string;
+  "FAILURE"?: string;
+}
+
+export type TResponse = {
+  status: number;
+  body: string;
+}
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+
+  return await resolveQuoteFromUrl(urls);
 };
+
+async function resolveQuoteFromUrl(urls: string[]): Promise<TResult[]> {
+
+  let requestPromises = [];
+  let responses: TResponse[];
+  for (let singleUrl of urls) {
+    requestPromises.push(httpGet(singleUrl));
+  }
+  responses = await Promise.all(requestPromises);
+  const finalResults: TResult[] = [];
+  for (let res of responses) {
+    const message = JSON.parse(res.body).message;
+    if (res.status === 200) {
+      finalResults.push({ "Arnie Quote": message });
+    } else {
+      finalResults.push({
+        "FAILURE": message
+      });
+    }
+  }
+  return finalResults;
+}
+
